0242d1b90deefc51e139c2c04245f3814cf8c9fe
Ethan Lee <elee@xpanxion.com>
2023-05-09 15:54:46 -0400

    Finish ex2

--- modern-javascript-for-react-devs/students/4/main.js
+++ modern-javascript-for-react-devs/students/4/main.js
@@ -3,6 +3,7 @@
 // Date: 5/24/2022
 //
 
+// ex1
 const countNumbers = (arr) => arr.length;
 
 //test cases
@@ -11,9 +12,20 @@ const array2 = [];
 const array3 = ["0"];
 console.log(countNumbers(array), countNumbers(array2), countNumbers(array3));
 
-const ex2 = () => {
-  // TODO...
+//ex2
+const minNumber = (arr) => {
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
 };
+//test cases
+console.log(minNumber([3, 5, 1, 2]));
+console.log(minNumber([10, 5, 3, 0]));
+console.log(minNumber([3, -29, 1, -100]));
 
 const ex3 = () => {
   // TODO...
