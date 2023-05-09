51dac194f984273cd1cc27b29d65c0bdb31ec772
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-05-09 15:47:44 -0800

    Ex 0

---
+++ modern-javascript-for-react-devs/students/13/main.js
@@ -0,0 +1,33 @@
+//
+// File: main.js
+// Date: 5/24/2022
+// 
+
+const ex1 = () => {
+    const array = [1, '2', 3, 'test', 1.2];
+    console.log(countNumbers(array));
+}
+
+const ex2 = () => {
+    // TODO...
+}
+
+const ex3 = () => {
+    // TODO...
+}
+
+//
+// Your functions here...
+//
+
+const countNumbers = (array) => {
+    let retval = 0;
+    // TODO...
+    return retval;
+}
+
+const main = async () => {
+    ex1();
+}
+
+main();
