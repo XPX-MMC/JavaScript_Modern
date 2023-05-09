53ee502582879f6bd1dc58a49504781bb4030b31
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-09 16:48:50 -0400

    Revert "Complete ex1"

--- modern-javascript-for-react-devs/students/8/main.js
+++ modern-javascript-for-react-devs/students/8/main.js
@@ -3,44 +3,31 @@
 // Date: 5/24/2022
 // 
 
-//EX 1
+const ex1 = () => {
+    const array = [1, '2', 3, 'test', 1.2];
+    console.log(countNumbers(array));
+}
+
+const ex2 = () => {
+    // TODO...
+}
+
+const ex3 = () => {
+    // TODO...
+}
+
+//
+// Your functions here...
+//
+
 const countNumbers = (array) => {
-    let count = 0;
+    let retval = 0;
+    // TODO...
+    return retval;
+}
 
-    for (let i = 0; i < array.length; i++){
-        if (typeof array[i] == 'number'){ 
-        count++; 
-        }
-    }
+const main = async () => {
+    ex1();
+}
 
-    return count;
-};
-
-const array = [1, '2', 3, 'test', 1.2];
-console.log(countNumbers(array));
-
-//EX 2
-// const ex2 = () => {
-//     // TODO...
-// }
-
-//EX 3
-// const ex3 = () => {
-//     // TODO...
-// }
-
-// //
-// // Your functions here...
-// //
-
-// const countNumbers = (array) => {
-//     let retval = 0;
-//     // TODO...
-//     return retval;
-// }
-
-// const main = async () => {
-//     ex1();
-// }
-
-// main();
+main();
