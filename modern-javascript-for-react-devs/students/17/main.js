f5e9eedb705544890b01f116e052a25225772305
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-05-09 16:53:33 -0400

    ex2

--- modern-javascript-for-react-devs/students/17/main.js
+++ modern-javascript-for-react-devs/students/17/main.js
@@ -1,44 +1,65 @@
 //
 // File: main.js
 // Date: 5/24/2022
-// 
+//
 
 const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
-}
+  const array = [1, "2", 3, "test", 1.2];
+  console.log(countNumbers(array));
+};
+
+// test cases for ex.1
+
+array1 = ["6", "valerie", false, 5, 3];
+array2 = [6, "sky", "sun", 5, "3"];
+array3 = ["valerie", true, 5, "3"];
 
 const ex2 = () => {
-    // TODO...
-}
+  const array = [12, 55, 2, 22, 11];
+  console.log(minNumber(array));
+  
+};
 
 const ex3 = () => {
-    // TODO...
-}
+  // TODO...
+};
 
 //
 // Your functions here...
 //
 
 const countNumbers = (array) => {
-    let retval = 0;
+  let retval = 0;
 
-    for (let i= 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] == "number") 
+      retval += 1;
+  }
 
-        if (typeof(array[i]) == 'number')
-            retval+= 1; 
-    }
-    
-    return retval;
-}
-// test cases for ex.1
+  return retval;
+};
 
-array1 = ["6", "valerie", false, 5, 3]
-array2 = [6, "sky","sun", 5, "3"]
-array3 = ["valerie", true, 5, "3"]
+
+const minNumber = (array) => {
+  let smallestNumber = 0;
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (i == 0) 
+      smallestNumber = array[0];
+      
+    else if (array[i] < smallestNumber) 
+      smallestNumber = array[i];
+  }
+
+  return smallestNumber;
+};
+
+
 
 const main = async () => {
-    ex1();
-}
+  ex1();
+  ex2();
+};
 
 main();
