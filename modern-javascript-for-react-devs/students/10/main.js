9bb418511f932d1eb3f9b95b2a2fea3c8392c97d
Michael Reid <mreid@xpanxion.com>
2023-05-09 21:44:20 -0400

    ex2 minNumber and Test Cases

--- modern-javascript-for-react-devs/students/10/main.js
+++ modern-javascript-for-react-devs/students/10/main.js
@@ -2,25 +2,42 @@
 // File: main.js
 // Date: 5/24/2022
 // 
-
-const ex1 = () => {
-    const array = [1, 2 , 4, 5, '2', 3, 'test', 5.7, 1.2];
-   // const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
-    
+const ex1 = () =>
+{
+    const array = [10, 20, 40,'65','tests', 5.9, 19];
+    //const array = [1, '2', 3, 'test', 5.7, 1.2];
+    console.log(countNumbers(array)); 
 }
 
-const ex2 = () => {
-    // TODO...
+const ex2 = () =>
+{
+    //const array1 = [12, 55, 2, 22, 11];
+    //const array1 = [100, 99,76,12,59,3000, .5]
+    const array1 = [4000, 3000,59,.3,76,99, .2]
+    console.log(minNumber(array1));
+}
+
+const minNumber = (array) => 
+{
+   let lowestNumber=array[0];
+    for (let i=1; i < array.length; i++)
+    {
+        if (lowestNumber >= array[i])
+        {
+            lowestNumber=array[i];
+            //console.log(lowestNumber, i);
+        }   
+    }
+    return lowestNumber;
 }
 
 const ex3 = () => {
     // TODO...
 }
 
-//
-// Your functions here...
-//
+
+ //Your functions here...
+
 
 const countNumbers = (array) => 
 {
@@ -42,7 +59,7 @@ const countNumbers = (array) =>
     return retval;
 }
 const main = async () => {
-    ex1();
+    ex2();
 }
 
 main();
