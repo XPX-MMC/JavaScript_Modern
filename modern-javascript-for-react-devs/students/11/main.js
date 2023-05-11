95d8cbd52b05f9a1f58ecbfacdd90543f2920c05
Nicole Portalatin <nportalatin@xpanxion.com>
2023-05-10 21:01:33 -0400

    Update Example 3

--- modern-javascript-for-react-devs/students/11/main.js
+++ modern-javascript-for-react-devs/students/11/main.js
@@ -17,11 +17,36 @@ const ex2 = () => {
 
 const ex3 = () => {
     // TODO...
+    const array1 = [1, 2, 3, 4, 5];
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(interleave(array1, array2));
+
 }
 
 //
 // Your functions here...
 //
+
+// function called interleave holding array1 and array 2
+const interleave = (array1, array2) => { 
+    // (.length is iterates thorugh the whole array)
+    // if array1 is not equal to array2 then it will return that message
+    if (array1.length !== array2.length) {
+
+    //will return the message below if array1 and 2 are not equal
+        return "ERROR: Array length mismatch";
+    }
+
+    let sameLength = " "; 
+
+    for (let i = 0; i < array1.length; i++ )
+    sameLength += array1[i] + array2[i]
+    return sameLength;
+} 
+
+
+
+
 const minNumber = (array) => { 
     let lowestNumber = array[0];
     
@@ -53,7 +78,7 @@ const countNumbers = (array) => {
 }
 // call your test cases
 const main = async () => {
-    ex2();
+    ex3();
 }
 
 main();
